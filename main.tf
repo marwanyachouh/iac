a117595853925323de8b151bf06c6505f9cd002e
Marwan Yachouh <marwan.yachouh@gmail.com>
2024-07-03 09:32:33 +0200

    Moved jenkinsfile to github

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "virtualbox" {
+  # Konfiguration för VirtualBox provider
+}
+
+resource "virtualbox_vm" "example" {
+  name      = "ansible-vm"
+  image     = "ubuntu/bionic64"
+  cpus      = 1
+  memory    = 1024
+  network_adapter {
+    type    = "hostonly"
+    hostonly_adapter = "vboxnet0"
+  }
+}
+
+output "vm_ip" {
+  value = virtualbox_vm.example.network_adapter[0].ipv4_address
+}
